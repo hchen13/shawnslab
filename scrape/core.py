2045b0f8ca6b347e48fbb3dbf230906782546481
Ethan <ethanbit@qq.com>
2017-10-10 18:17:16 +0800

    update

--- scrape/core.py
+++ scrape/core.py
@@ -1,10 +1,13 @@
 from time import sleep
 
+from pyvirtualdisplay import Display
 from selenium import webdriver
-
 from settings import *
 
 if __name__ == '__main__':
+	display = Display(visible=0, size=(800, 600))
+	display.start()
+
 	chrome = webdriver.Chrome()
 	chrome.get(URL_SHAWN_COMBO)
 	elem = chrome.find_element_by_css_selector("#cube-info > div.cube-blockmain > div > div.cube-profits.fn-clear > div:nth-child(3) > div.per")
