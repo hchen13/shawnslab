6ea356c49bb6cc1eb0b8f1713b6871804439eb93
Ethan <ethanbit@qq.com>
2017-10-10 18:08:31 +0800

    fix directory issue

--- scrape/core.py
+++ scrape/core.py
@@ -2,12 +2,12 @@ from time import sleep
 
 from selenium import webdriver
 
-from scrape.settings import *
+from settings import *
 
 if __name__ == '__main__':
 	chrome = webdriver.Chrome()
 	chrome.get(URL_SHAWN_COMBO)
 	elem = chrome.find_element_by_css_selector("#cube-info > div.cube-blockmain > div > div.cube-profits.fn-clear > div:nth-child(3) > div.per")
 	print(elem.text)
-	sleep(5)
+	sleep(2)
 	chrome.quit()
