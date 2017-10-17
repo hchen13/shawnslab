44975dabe6aea1b6032bca52348d746ee4382c7e
Ethan <ethanbit@qq.com>
2017-10-18 01:12:33 +0800

    update gitignore to ignore vim trashes; catch connection error and cancel the operation for once

--- scrape/app.py
+++ scrape/app.py
@@ -3,57 +3,61 @@ import sys
 from datetime import datetime
 from time import sleep
 
+from sqlalchemy.exc import OperationalError
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import scrape.core as core
-from sqlalchemy.exc import OperationalError
+
 
 def is_every_n_minutes(n_minutes):
-	fired = False
-	def check(time):
-		nonlocal fired
-		if time.minute % n_minutes == 0 and not fired:
-			fired = True
-			return True
-		elif time.minute % n_minutes != 0:
-			fired = False
-		return False
-	return check
+    fired = False
+
+    def check(time):
+        nonlocal fired
+        if time.minute % n_minutes == 0 and not fired:
+            fired = True
+            return True
+        elif time.minute % n_minutes != 0:
+            fired = False
+        return False
+    return check
 
 
 def main():
-	try:
-		Session = core.init_db()
-	except OperationalError as e:
-		print("数据库初始化失败: {}".format(e))
-		print("程序即将终止")
-		exit()
+    try:
+        Session = core.init_db()
+    except OperationalError as e:
+        print("数据库初始化失败: {}".format(e))
+        print("程序即将终止")
+        exit()
 
-	frequency = is_every_n_minutes(2)
+    frequency = is_every_n_minutes(5)
 
-	while True:
+    while True:
 
-		current = datetime.now()
+        current = datetime.now()
 
-		if frequency(current):
-			nv = core.extract_data()
-			core.save(nv, Session)
+        if frequency(current):
+            nv = core.extract_data()
+            if nv is not None:
+                core.save(nv, Session)
 
-		sys.stdout.flush()
-		sleep(30)
+        sys.stdout.flush()
+        sleep(30)
 
 
 def run_once():
-	try:
-		Session = core.init_db()
-	except OperationalError as e:
-		print("数据库初始化失败: {}".format(e))
-		print("程序即将终止")
-		exit()
+    try:
+        Session = core.init_db()
+    except OperationalError as e:
+        print("数据库初始化失败: {}".format(e))
+        print("程序即将终止")
+        exit()
 
-	nv = core.extract_data()
-	core.save(nv, Session)
+    nv = core.extract_data()
+    core.save(nv, Session)
 
 
 if __name__ == '__main__':
-	run_once()
+    run_once()
--- scrape/core.py
+++ scrape/core.py
@@ -15,69 +15,79 @@ from scrape.settings import *
 
 
 def extract_data():
-	print("当前时间: {}\t正在提取净值...".format(datetime.now()))
-	headers = {
-		'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
-	}
-	response = requests.get(URL_SHAWN_COMBO, headers=headers)
-	content = response.content
-	page = BeautifulSoup(content, "lxml")
-	try:
-		elem = page.select("#cube-info > div.cube-blockmain > div > div.cube-profits.fn-clear > div:nth-of-type(3) > div.per")[0]
-		value = elem.text
-		print("提取完成, 此时净值为{}\n".format(value))
-	except IndexError as e:
-		print("净值元素在页面中不存在, 跳过...\n")
-		value = None
-	return float(value) if value else None
+    print("当前时间: {}\t正在提取净值...".format(datetime.now()))
+    headers = {
+            'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    }
+    try:
+        response = requests.get(URL_SHAWN_COMBO, headers=headers)
+    except ConnectionError as e:
+        print("网络连接错误: {}".format(e))
+        print("提取失败, 取消本次操作\n")
+        return None
+    content = response.content
+    page = BeautifulSoup(content, "lxml")
+    try:
+        elem = page.select(
+                "#cube-info > div.cube-blockmain > div > div.cube-profits.fn-clear > div:nth-of-type(3) > div.per")[0]
+        value = elem.text
+        print("提取完成, 此时净值为{}\n".format(value))
+    except IndexError as e:
+        print("净值元素在页面中不存在, 跳过...\n")
+        value = None
+    return float(value) if value else None
 
 
 Base = declarative_base()
 
+
 class NetValue(Base):
-	__tablename__ = 'netvalue'
+    __tablename__ = 'netvalue'
 
-	id = Column(Integer, primary_key=True)
-	value = Column(Float)
-	record_time = Column(DateTime, default=datetime.now)
-	date = Column(Date)
+    id = Column(Integer, primary_key=True)
+    value = Column(Float)
+    record_time = Column(DateTime, default=datetime.now)
+    date = Column(Date)
 
-	def __repr__(self):
-		date = self.record_time.date()
-		return "{} 净值: {}".format(self.date, self.value)
+    def __repr__(self):
+        date = self.record_time.date()
+        return "{} 净值: {}".format(self.date, self.value)
+
 
 def init_db():
-	print("正在初始化数据库...")
-	url = '{dialect}+{driver}://{username}:{password}@{host}/{db}?charset=utf8'.format(
-		dialect='mysql', driver='mysqldb',
-		username=DB_USER, password=DB_PASS,
-		host=DB_HOST, db=DB_NAME
-	)
-	engine = create_engine(url)
-	Base.metadata.create_all(engine)
-	Session = sessionmaker(bind=engine)
-	print('数据库初始化完成\n')
-	return Session
+    print("正在初始化数据库...")
+    url = '{dialect}+{driver}://{username}:{password}@{host}/{db}?charset=utf8'.format(
+        dialect='mysql',
+        driver='mysqldb',
+        username=DB_USER,
+        password=DB_PASS,
+        host=DB_HOST,
+        db=DB_NAME)
+    engine = create_engine(url)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    print('数据库初始化完成\n')
+    return Session
 
 
 def save(x, Session):
-	print("正在储存数据...")
-	session = Session()
-	today = datetime.now().date()
-	result = session.query(NetValue).filter(NetValue.date == today).first()
-	if result is None:
-		print("正在储存{}新数据: {}".format(today, x))
-		nv = NetValue(value=x, date=today)
-		session.add(nv)
-	else:
-		print("正在更新{}原有数据: {} - {}".format(today, result.value, x))
-		result.value = x
-		result.record_time = datetime.now()
-	session.commit()
-	print('储存成功!\n')
+    print("正在储存数据...")
+    session = Session()
+    today = datetime.now().date()
+    result = session.query(NetValue).filter(NetValue.date == today).first()
+    if result is None:
+        print("正在储存{}新数据: {}".format(today, x))
+        nv = NetValue(value=x, date=today)
+        session.add(nv)
+    else:
+        print("正在更新{}原有数据: {} - {}".format(today, result.value, x))
+        result.value = x
+        result.record_time = datetime.now()
+    session.commit()
+    print('储存成功!\n')
 
 
 if __name__ == '__main__':
-	Session = init_db()
-	a = extract_data()
-	save(a, Session)
+    Session = init_db()
+    a = extract_data()
+    save(a, Session)
