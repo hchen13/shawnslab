07d126774ca888a33431d3c17c20590adf5a3da5
Ethan Chen <gradschool.hchen13@gmail.com>
2018-01-17 11:46:12 +0800

    update version: adding feature - cli command  to scan and update the history once

--- scrape/settings.py
+++ scrape/settings.py
@@ -5,10 +5,10 @@ URL_SHAWN_COMBO = "https://xueqiu.com/P/ZH946285"
 URL_ALL = "https://xueqiu.com/cubes/nav_daily/all.json?cube_symbol=ZH946285"
 
 try:
-	DB_USER = os.environ['SHAWN_DB_USER']
-	DB_PASS = os.environ['SHAWN_DB_PASS']
-	DB_HOST = os.environ['SHAWN_DB_HOST']
-	DB_NAME = os.environ['SHAWN_DB_NAME']
+    DB_USER = os.environ['SHAWN_DB_USER']
+    DB_PASS = os.environ['SHAWN_DB_PASS']
+    DB_HOST = os.environ['SHAWN_DB_HOST']
+    DB_NAME = os.environ['SHAWN_DB_NAME']
 except KeyError as e:
-	print("请在环境变量中设置数据库信息: ", e)
-	exit()
+    print("请在环境变量中设置数据库信息: ", e)
+    exit()
